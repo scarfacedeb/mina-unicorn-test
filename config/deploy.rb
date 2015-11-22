9fb890c3a40135134714e2588a0d20a63e80f273
Andrew Volozhanin <scarfacedeb@gmail.com>
2015-11-22 20:29:15 +0500

    Add tmp to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :user, "deploy"
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, []
+set :shared_paths, ["tmp/"]
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
