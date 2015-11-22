d66e7c38e637b4ad37113aa84702d606d675cd79
Andrew Volozhanin <scarfacedeb@gmail.com>
2015-11-22 21:23:49 +0500

    Add pid file config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,9 @@ listen "127.0.0.1:3000"
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
 
+shared_dir = File.expand_path('../../../shared/', __FILE__)
+pid "#{shared_dir}/pids/unicorn.pid"
+
 before_fork do |server, worker|
 	  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 end
